57e55777f87c20bd8bd19407368f2323568eee9a
meadowse <meadowse@student.21-school.ru>
2025-02-26 12:48:18 +0300

    fixExpired

--- src/backPlanner/api/views.py
+++ src/backPlanner/api/views.py
@@ -75,8 +75,8 @@ def getAgreements(request):
             dateOfStart = {'dateOfStart': {'title': '', 'value': obj.get('dateOfStart')}}
             obj.update(dateOfStart)
             dateOfEnding = obj.get('dateOfEnding')
-            print(type(dateOfEnding))
-            print(type(today))
+            print('dateOfEnding: ' + type(dateOfEnding))
+            print('today: ' + type(today))
             if status == 'В работе' and dateOfEnding < today:
                 dateOfEnding = {
                     'dateOfEnding': {'title': 'Срок работы', 'value': obj.get('dateOfEnding'), 'expired': True}}
