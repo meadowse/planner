a0e4216cbc7e537207f5d8b913defd9b9df61807
meadowse <baha.199620@gmail.com>
2025-08-20 15:56:35 +0300

    fix

--- src/backPlanner/api/views.py
+++ src/backPlanner/api/views.py
@@ -589,7 +589,7 @@ def editTask(request):
         comment = obj.get('comment')
         typeWorkId = obj.get('typeWorkId')
         dateStart = obj.get('dateStart')
-        deadline = obj.get('deadline').get('value')
+        deadline = obj.get('deadline')
         directorId = obj.get('directorId')
         executorId = obj.get('executorId')
         done = obj.get('done')
