80b2626e799a0f82019643e18e46a160c7aa67a0
meadowse <baha.199620@gmail.com>
2025-08-28 16:33:11 +0300

    fix

--- src/backPlanner/api/views.py
+++ src/backPlanner/api/views.py
@@ -46,7 +46,6 @@ def getAgreements(request):
         LEFT JOIN T218 ON T218.F4691 = T212.ID
         LEFT JOIN T3 director ON T218.F4693 = director.ID
         LEFT JOIN T3 executor ON T218.F4694 = executor.ID
-        -- WHERE T212.ID > 3025
         GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15
         """  # F4648 - путь, F4538 - номер договора, F4544 - стадия, F4946 - адрес, F4948 - направление, F4566 - дата окончания
         cur.execute(sql)
@@ -759,7 +758,6 @@ def getAllDepartmentsStaffAndTasks(request):
             LEFT JOIN T3 director ON T218.F4693 = director.ID
             LEFT JOIN T3 executor ON T218.F4694 = executor.ID
             WHERE T3.F5383 = 1
-            -- AND T212.ID >= 3000
             GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11) tasks GROUP BY 1, 2, 3, 4, 5
             """
             cur.execute(sql)
