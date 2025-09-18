2d98d9278ab73eab57d3e97b5962f9cdf1b7ec21
meadowse <baha.199620@gmail.com>
2025-09-18 16:32:48 +0300

    fixDocker

--- src/backPlanner/config/gunicorn/dev.py
+++ src/backPlanner/config/gunicorn/dev.py
@@ -5,7 +5,7 @@ import multiprocessing
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "planner.wsgi:application"
 # The granularity of Error log outputs
-loglevel = "debug"
+# loglevel = "debug"
 # The number of worker processes for handling requests
 workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
@@ -13,10 +13,10 @@ bind = "0.0.0.0:8000"
 # Restart workers when code changes (development only!)
 # reload = True
 # Write access and error info to /var/log
-accesslog = errorlog = "/app/dev.log"
+# accesslog = errorlog = "/app/dev.log"
 # Redirect stdout/stderr to log file
 # capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/app/dev.pid"
+# pidfile = "/app/dev.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 # daemon = True
